82059e509d4b3e88d2a2f77e77afa9a1fb3e3234
RSWilson1 <robert.s.wilson1996@gmail.com>
2022-05-06 12:44:22 +0100

    Added models

--- Ploutos/dashboard/models.py
+++ Ploutos/dashboard/models.py
@@ -1,83 +1,77 @@
 from django.db import models
 
 # Create your models here.
-class storage_costs(models.Model):
-    name = models.CharField(max_length=100, blank = True, null = True)
-    project_id = models.IntegerField(blank = True, null = True)
-    # date = models.DateField(blank = True, null = True)
-    storage_data = models.FloatField(blank = True, null = True)
-    archival_state = models.IntegerField(blank = True, null = True)
+class Users(models.Model):
+    """Model representing an org user"""
+    user_name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.name
+        return self.user_name
 
-
-class projects(models.Model):
-    dx_id = models.CharField(max_length=100, blank = False, null = False)
-    name = models.CharField(max_length=200, blank = False, null = False)
-    created = models.IntegerField(blank = False, null = False)
-    created_by = models.IntegerField(blank = False, null = False)
-
-    def __str__(self):
-        return self.name
-
-
-class daily_org_running_total(models.Model):
-    storage_charges = models.FloatField(blank = False, null = False)
-    compute_charges = models.FloatField(blank = False, null = False)
-    egress_charges = models.FloatField(blank = False, null = False)
-    storage_charges = models.FloatField(blank = False, null = False)
-    date_id = models.IntegerField(blank = False, null = False)
+class Projects(models.Model):
+    """Model representing a project."""
+    dx_id = models.CharField(max_length=35, unique=True)
+    name = models.CharField(max_length=100)
+    created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+    created = models.ForeignKey(Dates, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.dx_id
+
+class Dates(models.Model):
+    """Model representing a date.
+    N.B. date has to be in YYYY-MM-DD format"""
+    date = models.DateField()
+
+class DailyOrgRunningTotal(models.Model):
+    """Model representing running totals for the org"""
+    date = models.ForeignKey(Dates, on_delete=models.CASCADE, unique=True)
+    storage_charges = models.FloatField()
+    compute_charges = models.FloatField()
+    egress_charges = models.FloatField()
+    estimated_balance = models.FloatField()
 
 
-class dates(models.Model):
-    date = models.IntegerField(blank = False, null = False)
+class StorageCosts(models.Model):
+    """Model representing storage costs per project"""
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE, unique=True)
+    unique_size_live = models.FloatField()
+    unique_size_archived = models.FloatField()
+    total_size_live = models.FloatField()
+    total_size_archived = models.FloatField()
+    unique_cost_live = models.FloatField()
+    unique_cost_archived = models.FloatField()
+    total_cost_live = models.FloatField()
+    total_cost_archived = models.FloatField()
+    date = models.ForeignKey(Dates, on_delete=models.CASCADE, unique=True)
 
-    def __str__(self):
-        return self.name
+########
 
+# class compute_costs(models.Model):
+#     executable_id = models.IntegerField()
+#     project_id = models.IntegerField()
+#     # intstance_id = models.IntegerField(blank = True, null = True)
+#     runtime = models.IntegerField()
+#     total_cost = models.FloatField()
+#     launched_by = models.IntegerField()
+#     date_id = models.IntegerField()
 
-class users(models.Model):
-    user_name = models.CharField(max_length=100, blank = False, null = False)
-
-    def __str__(self):
-        return self.name
-
-
-class compute_costs(models.Model):
-    executable_id = models.IntegerField()
-    project_id = models.IntegerField()
-    # intstance_id = models.IntegerField(blank = True, null = True)
-    runtime = models.IntegerField()
-    total_cost = models.FloatField()
-    launched_by = models.IntegerField()
-    date_id = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-
-class executables(models.Model):
-    executable_id = models.IntegerField()
-    dx_id = models.CharField(max_length=200, blank = False, null = False)
-    excutable_name = models.CharField(max_length=200, blank = False, null = False)
-    version = models.CharField(max_length=30, blank = False, null = False)
-
-    def __str__(self):
-        return self.name
-
-
-# class AllFields(models.Model):
-#     name = models.CharField(max_length=50, blank = True, null = True)
-#     slug = models.SlugField(default = 'test')
-#     date = models.DateField(blank = True, null = True)
-#     float = models.FloatField(blank = True, null = True)
-#     json = models.JSONField()
-#     id_for_something = models.PositiveIntegerField() # Zero - 9223372036854775807 accepted
-#     unique_id = models.UUIDField() #Used for postgres and other uses.
-#     # More here: https://docs.djangoproject.com/en/4.0/ref/models/fields/
 #     def __str__(self):
 #         return self.name
+
+# class executables(models.Model):
+#     executable_id = models.IntegerField()
+#     dx_id = models.CharField(max_length=200, blank = False, null = False)
+#     excutable_name = models.CharField(max_length=200, blank = False, null = False)
+#     version = models.CharField(max_length=30, blank = False, null = False)
+
+#     def __str__(self):
+#         return self.name
+
+# class Egress(models.Model):
+#     project = models.ForeignKey(Projects,on_delete=models.CASCADE)
+#     egress_cost = models.FloatField()
+#     date = models.ForeignKey(Dates, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.project
