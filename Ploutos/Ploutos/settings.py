edc53366625b1cc6d7d4fd4f5f0d4c6c203c0126
RSWilson1 <robert.s.wilson1996@gmail.com>
2022-05-06 12:34:49 +0100

    Added Secret key to settings

--- Ploutos/Ploutos/settings.py
+++ Ploutos/Ploutos/settings.py
@@ -24,6 +24,7 @@ DB_PASSWORD = CREDENTIALS['DB_PASSWORD']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = CREDENTIALS["SECRET_KEY"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
