15ca0762465f83494363b056cba1e1400bca30c8
RSWilson1 <robert.s.wilson1996@gmail.com>
2022-05-12 09:51:45 +0100

    Updated populate_db script to add project ids and use foreign keys

--- Ploutos/scripts/populate_db.py
+++ Ploutos/scripts/populate_db.py
@@ -48,50 +48,14 @@ def populate_projects():
         user, created = Users.objects.get_or_create(
             user_name=project_dict['createdBy']['user'],)
 
-        # Adding dates
+        # Adding dates to table
         a_new_date, created = Dates.objects.get_or_create(
             date=datetime.datetime.fromtimestamp(project_dict['created']/1000).strftime('%Y-%m-%d'),)
 
-# for index, row in projects_df.iterrows():
-#             #Add users to users table
-#             user, created = Users.objects.get_or_create(
-#                 user_name = row['created_by'],
-#             )
-
-#             # Add project created dates to Dates table to make IDs
-#             a_new_date, created = Dates.objects.get_or_create(
-#                 date = row['created'],
-#             )
-
-#             # Filtering for updating existing project names in table in case they have been changed in DNAnexus
-#             # Get all the project objects
-#             projects_data = Projects.objects.all()
-
-#             # Get names of projects from csv
-#             new_name = row['name']
-
-#             # Dict to filter projects using dx_id from csv
-#             filter_dict = {
-#                 "dx_id": row['dx_id'],
-#             }
-
-#             # Filter projects in db by dx_id from csv
-#             found_row = projects_data.filter(**filter_dict)
-
-#             # If already in database, get the name
-#             if found_row:
-#                 existing_project = found_row.values_list(
-#                     "name", flat=True
-#                 ).get()
-
-#                 # If the existing proj name is not same as name in csv update it
-#                 if existing_project != new_name:
-#                     found_row.update(name=new_name)
-
-#             # Get or create project objects
-#             project, created = Projects.objects.get_or_create(
-#                 dx_id = row['dx_id'],
-#                 name = row['name'],
-#                 created_by = user,
-#                 created = a_new_date,
-#             )
+        # Adding project details to table
+        project, created = Projects.objects.update_or_create(
+                dx_id = project_dict['id'],
+                name = project_dict['name'],
+                created_by = user,
+                created = a_new_date,
+            )
