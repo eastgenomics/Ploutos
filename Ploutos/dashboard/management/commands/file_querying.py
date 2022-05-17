3f4ba5665768a5f0fc4e824bd87ca204713e5d54
rklocke <rebeccakatelocke@gmail.com>
2022-05-17 15:15:46 +0100

    Changed to looping over projects with only a subset of fields

--- Ploutos/dashboard/management/commands/file_querying.py
+++ Ploutos/dashboard/management/commands/file_querying.py
@@ -7,8 +7,8 @@ import pandas as pd
 import time
 import dxpy as dx
 
-from collections import defaultdict
 from calendar import monthrange
+from collections import defaultdict
 from dashboard.models import Users, Projects, Dates, DailyOrgRunningTotal
 from django.apps import apps
 from django.conf import settings
@@ -54,26 +54,43 @@ class Command(BaseCommand):
             seen_twice = set(x for x in file_ids if x in seen or seen_add(x))
             return list(seen_twice)
 
+        def get_projects():
+            """Get all projects in DNAnexus"""
+            project_response = list(dx.find_projects(billed_to='org-emee_1', level='VIEW', describe=True))
+
+            project_ids_list = []
+
+            all_projects = defaultdict(dict)
+            for project in project_response:
+                project_id = project['id']
+                project_ids_list.append(project_id)
+                all_projects[project_id]['dx_id'] = project['describe']['id']
+                all_projects[project_id]['name'] = project['describe']['name']
+                all_projects[project_id]['created_by'] = project['describe']['createdBy']['user']
+                all_projects[project_id]['created_epoch'] = project['describe']['created']
+                all_projects[project_id]['created'] = dt.datetime.fromtimestamp((project['describe']['created']) / 1000).strftime('%Y-%m-%d')
+            return all_projects, project_ids_list
+
 
         def get_files():
-            """Gets all files and their info and puts into a nested dict"""
-
-            # This takes like 1.9 hours
+            """Gets files per proj and their info and puts into a nested dict"""
+            project_list = get_projects()[1]
             print("Starting to look for all files in DNAnexus")
             start = time.time()
-            files = list(dx.search.find_data_objects(classname='file', describe=True))
+
+            files_per_proj_dict = defaultdict(lambda: {"files": []})
+            for proj in project_list:
+                files = list(dx.search.find_data_objects(classname='file', project = proj, describe={'fields':{'archivalState': True, 'size': True, 'name': True}}))
+                for file in files:
+                    #file_id_list.append(d['id'])
+                    proj = file['project']
+                    files_per_proj_dict[proj]["files"].append({"id": file["id"], "name": file["describe"]['name'], "size": file.get('describe',{}).get('size',0), "state": file['describe']['archivalState']})
+
             end = time.time()
             total_time = end - start
             print(f"Total time to get all files was {total_time}")
 
-            file_dct = defaultdict(lambda: {"data": []})
-
-            for d in files:
-                proj = d['project']
-                file_dct[proj]["data"].append({"file_id": d["id"], "name": d["describe"]['name'], 
-                "size": d.get('describe',{}).get('size',0), "state": d['describe']['archivalState']})
-            
-            return file_dct
+            return files_per_proj_dict
 
         def calculate_total():
             """Calculates total cost per project (not taking into account duplicates) currently"""
@@ -81,24 +98,28 @@ class Command(BaseCommand):
             live_storage_cost_month = settings.STORAGE_COST_MONTH
             # This is an arbitrary number until Wook provides it
             archived_storage_cost_month = 0.0014
-            # Get the files dict with the get_files func
-            file_info_dict = get_files()
+
             # Find days in month to work out daily charge
             days_in_month = no_of_days_in_month(self.year, self.month)
+            today_date = self.today_date
 
-            # For each project, add up the sizes of the files 
+            # Get the files into a dict
+            files_per_proj_dict = get_files()
+
+            # For each project, total the sizes of the files 
             # Then convert to GB, multiply by cost per month and divide by days in month to get daily charge estimate
+
             files_dict = {}
             list_of_files_dicts = []
-            for k, v in file_info_dict.items():
-                total_size_live = sum([x['size'] for x in v['data'] if x['state'] != "archived"])
-                total_size_archived = sum([x['size'] for x in v['data'] if x['state'] == "archived"])
+            for k, v in files_per_proj_dict.items():
+                total_size_live = sum([x['size'] for x in v['files'] if x['state'] == "live"])
+                total_size_archived = sum([x['size'] for x in v['files'] if x['state'] != "live"])
                 total_cost_live = (((total_size_live / (2**30)) * live_storage_cost_month) / days_in_month)
                 total_cost_archived = (((total_size_archived / (2**30)) * archived_storage_cost_month) / days_in_month)
-                
+
                 files_dict = {"project": k, "total_size_live": total_size_live, "total_size_archived": total_size_archived, 
-                            "total_cost_live": total_cost_live, "total_cost_archived": total_cost_archived, 'date':self.today_date}
-                
+                            "total_cost_live": total_cost_live, "total_cost_archived": total_cost_archived, 'date':today_date}
+
                 list_of_files_dicts.append(files_dict)
 
             return list_of_files_dicts
