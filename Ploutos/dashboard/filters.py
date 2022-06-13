704d116083a1041a55676115751ab4e03df35356
rklocke <rebeccakatelocke@gmail.com>
2022-06-13 15:36:06 +0100

    Deleted filters.py

--- Ploutos/dashboard/filters.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-
-from dashboard.models import *
-
-class StorageCostsFilter(django_filters.FilterSet):
-    class Meta:
-        model = StorageCosts
-        fields = '__all__'
