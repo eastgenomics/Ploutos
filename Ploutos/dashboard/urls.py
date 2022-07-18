aacc3813916f795ac30cbc903032591f6217de15
RSWilson1 <robert.s.wilson1996@gmail.com>
2022-07-18 12:42:10 +0100

    Adding leaderboard page

--- Ploutos/dashboard/urls.py
+++ Ploutos/dashboard/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('storage/', views.storage_chart, name='storage'),
-    path('jobs/', views.jobs, name='jobs')
+    path('compute/', views.compute_graph, name='compute'),
+    path('Leaderboard/', views.compute_graph, name='leaderboard'),
 ]
